559a81b9aeafe9987e3586ebf38c4e00a0b21bf5
Hitarth Narvala <44180325+hitt08@users.noreply.github.com>
2023-08-18 17:50:31 +0100

    Create oc_env.sh

---
+++ oc_env.sh
@@ -0,0 +1,8 @@
+#REQUIRED
+export OC_PROJECT=
+export NFS_CLAIM_PATH=
+
+#OPTIONAL -- DEFAULTS
+export DEFAULT_DOCKER_IMAGE="pytorch/pytorch:2.0.1-cuda11.7-cudnn8-runtime"
+export DEFAULT_CPU=4000
+export DEFAULT_MEM=16
